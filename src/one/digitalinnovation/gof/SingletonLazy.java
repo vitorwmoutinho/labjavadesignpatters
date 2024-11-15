d0a23a81a4f0decfa3a864ee11d501cb1fd06454
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-15 16:36:50 -0300

    Aprendendo Design patterns com Java

---
+++ src/one/digitalinnovation/gof/SingletonLazy.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof;
+/*
+     * Singleton "preguiçoso"
+     * @author falvojr
+     */
+    
+public class SingletonLazy {
+    private static SingletonLazy instancia;
+
+    private SingletonLazy(){
+        super();
+    }
+
+    public static SingletonLazy getInstancia(){
+        if(instancia == null){
+            instancia= new SingletonLazy();
+        }
+        return instancia;
+    }
+
+}
---
+++ src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.gof;
+
+public class Test {
+   public static void main(String[] args) {
+    SingletonLazy
+    
+   } 
+
+}
